5e2329fe08f6473065872f911c525740be647d42
dog-broad <rushyendra.7004@gmail.com>
2024-03-20 23:18:59 +0530

    Update API headers in ProfilePage.js

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -35,8 +35,9 @@ function ProfileForm() {
       fetch(`https://clist.by:443/api/v4/account/?resource=leetcode.com&handle=${userData.username}`, {
         method: 'GET',
         headers: {
-          "Accept": 'application/json',
-          "Allow-Control-Allow-Origin": "*",
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
         },
       },
       ).then(response => {
