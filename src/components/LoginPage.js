9733b842ea1fee2b6d9eb3db178bde545e982c4a
dog-broad <rushyendra.7004@gmail.com>
2024-03-23 15:57:12 -0700

    Add particle effects to loading screen and login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,9 +3,11 @@ import { signInWithPopup, signOut, getAuth } from "firebase/auth";
 import { auth, googleProvider } from '../services/firebase';
 import 'firebaseui/dist/firebaseui.css'; // Import Firebase UI CSS
 import LoadingScreen from './LoadingScreen';
+import { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 import './LoginPage.css'; // Import custom CSS for styling
 
-const delay = 2000; // milliseconds
+const delay = 10000; // milliseconds
 
 function LoginPage() {
   const [isLoading, setIsLoading] = useState(true);
@@ -21,6 +23,10 @@ function LoginPage() {
       setIsLoading(false);
     }
     setTimeout(checkLogin, delay);
+
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    });
   }, []);
 
   const signInWithGoogle = async () => {
@@ -39,23 +45,53 @@ function LoginPage() {
     }
   };
 
-  if (isLoading) {
-    return <LoadingScreen />;
-  } else if (user) {
-    return (
-      <div className="login-container">
-        <p className="welcome-message">Welcome, {user.displayName}!</p>
-        <button onClick={handleSignOut} className="sign-out-button">Sign out</button>
+  const particlesOptions = {
+    // Other options...
+    detectRetina: true,
+    motion: {
+      reduce: {
+        factor: 4,
+        value: true
+      }
+    },
+    particles: {
+      // Other particle options...
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "none",
+        random: true,
+        straight: false,
+        outModes: {
+          default: "out"
+        },
+        attract: {
+          enable: false,
+          rotate: {
+            x: 600,
+            y: 1200
+          }
+        }
+      }
+    }
+  };
+
+  return (
+    <div className="login-container">
+      {isLoading && <LoadingScreen />}
+      <div id="particles-js" className="particles-js">
+        <canvas className="particles-canvas"></canvas>
       </div>
-    );
-  } else {
-    return (
-      <div className="login-container">
-        <div id="firebaseui-auth-container"></div>
-        <button onClick={signInWithGoogle} className="sign-in-button">Sign in with Google</button>
-      </div>
-    );
-  }
+      <div id="firebaseui-auth-container"></div>
+      <button onClick={signInWithGoogle} className="sign-in-button">Sign in with Google</button>
+      {!isLoading && user && (
+        <>
+          <p className="welcome-message">Welcome, {user.displayName}!</p>
+          <button onClick={handleSignOut} className="sign-out-button">Sign out</button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default LoginPage;
